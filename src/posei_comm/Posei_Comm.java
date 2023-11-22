b4be55bfbfc0b1a6b6eb8ad15b082fda50678071
Kavin-T <kavin14edu@gmail.com>
2023-11-22 22:44:44 +0530

    added files

---
+++ src/posei_comm/Posei_Comm.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package posei_comm;
+
+/**
+ *
+ * @author kavin
+ */
+public class Posei_Comm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
